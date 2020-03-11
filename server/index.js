4084dbcf9f5f86779a4606722430a1ac8cb1c43e
viktorostlund <viktor.ostlund@appliedtechnology.se>
2020-03-11 18:52:49 +0100

    add basic boilerplates, check that api works

--- server/index.js
+++ server/index.js
@@ -2,17 +2,33 @@ const express = require('express');
 const app = express();
 const axios = require('axios');
 const bodyParser = require('body-parser');
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const fs = require('fs');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send('hej');
-});
+let words = ['hej', 'kul', 'bra'];
 
+server.listen(2000);
+
+//Current game
 app.get('/api/:query', async (req, res) => {
     const answer = await getNewsNumber(req.params.query);
     res.json(answer);
+});
+
+io.on('connection', function (socket) {
+    setInterval(() => {
+        socket.emit('current score', words);
+    }, 1000);
+    socket.on('player answer', function (answer) {
+        //Calculate points för length of word and trendiness based on API
+        words.push(answer);
+        const jsonWords = JSON.stringify(words);
+        fs.appendFile('db.json', jsonWords, () => console.log('Hej') );
+    });
 });
 
 app.listen(3000);
